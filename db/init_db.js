dd3c0a38518335f0297f570642b0a4ae12e3bb95
murdybirdy <murdockdm@outlook.com>
2023-06-21 19:51:53 -0600

    drop tables and create tables good to go in the init_db.js

--- db/init_db.js
+++ db/init_db.js
@@ -9,8 +9,41 @@ async function buildTables() {
     client.connect();
 
     // drop tables in correct order
+      await client.query(/*sql*/`
+        DROP TABLE IF EXISTS cart;
+        DROP TABLE IF EXISTS products;
+        DROP TABLE IF EXISTS users;
+      `);
 
     // build tables in correct order
+      await client.query(/*sql*/`
+        CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          username VARCHAR(255) UNIQUE NOT NULL,
+          password VARCHAR(255) NOT NULL
+        );
+      `);
+
+      await client.query(/*sql*/`
+        CREATE TABLE products (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) UNIQUE NOT NULL,
+          category VARCHAR(255) NOT NULL,
+          description VARCHAR(255) NOT NULL,
+          price INTEGER NOT NULL
+        );
+      `);
+
+      await client.query(/*sql*/`
+        CREATE TABLE cart (
+          id SERIAL PRIMARY KEY,
+          "userId" INTEGER REFERENCES users(id),
+          "productId" INTEGER REFERENCES products(id),
+          quantity INTEGER,
+          UNIQUE ("userId", "productId")
+        );
+      `);
+
   } catch (error) {
     throw error;
   }
