b6879ca0a52f957a1c85b1f2077ba715396b6acd
murdybirdy <murdockdm@outlook.com>
2023-06-21 19:12:24 -0600

    initializing project

--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,11 @@
 // grab our db client connection to use with our adapters
 const client = require('../client');
 
+async function getAllUsers() {
+  /* this adapter should fetch a list of users from your db */
+}
+
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
 };
-
-async function getAllUsers() {
-  /* this adapter should fetch a list of users from your db */
-}
