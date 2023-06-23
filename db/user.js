db2352698f1e61cb569e1ddb1f3967963123a97e
Westin <wgneiting1@gmail.com>
2023-06-22 18:41:23 -0600

    db users

--- db/user.js
+++ db/user.js
@@ -1,11 +1,29 @@
 // grab our db client connection to use with our adapters
-const client = require('../client');
+const client = require('./client');
 
 async function getAllUsers() {
-  /* this adapter should fetch a list of users from your db */
+  try {
+    const query = 'SELECT * FROM users;';
+    const result = await client.query(query);
+    return result.rows;
+  } catch (error) {
+    throw new Error('Error fetching users from the database.');
+  }
+}
+
+async function getUserById(id) {
+  try {
+    const query = 'SELECT * FROM users WHERE id = $1;';
+    const result = await client.query(query, [id]);
+    return result.rows[0];
+  } catch (error) {
+    throw new Error(`Error fetching user with id ${id} from the database.`);
+  }
 }
 
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
+  getUserById
+
 };
