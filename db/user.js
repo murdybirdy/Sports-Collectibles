49d5a1b0e89db4f0fffdff5e34ae4308fab06f99
Westin <wgneiting1@gmail.com>
2023-06-22 20:38:43 -0600

    getuser and getuserbyusername in db user

--- db/user.js
+++ db/user.js
@@ -1,5 +1,6 @@
 // grab our db client connection to use with our adapters
 const client = require('./client');
+const bcrypt = require('bcrypt');
 
 async function getAllUsers() {
   try {
@@ -8,6 +9,32 @@ async function getAllUsers() {
     return result.rows;
   } catch (error) {
     throw new Error('Error fetching users from the database.');
+  }
+}
+
+async function getUser({ username, password }) {
+  try {
+    const user = await getUserByUsername(username);
+    const hashedPassword = user.password;
+    const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+
+    if (passwordsMatch) {
+      delete user.password;
+      return user;
+    }
+  } catch (error) {
+    console.log('Error getting user', error);
+    throw error;
+  }
+}
+
+async function getUserByUsername(username) {
+  try {
+    const query = 'SELECT * FROM users WHERE username = $1;';
+    const result = await client.query(query, [username]);
+    return result.rows[0];
+  } catch (error) {
+    throw new Error(`Error fetching user with username ${username} from the database.`);
   }
 }
 
@@ -36,6 +63,9 @@ async function createUser(username, password) {
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
+  getUser,
+  getUserByUsername,
   getUserById,
-  createUser
+  createUser,
+
 };
