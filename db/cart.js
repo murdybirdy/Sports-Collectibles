b9b9fbad4cfdbcde1191a12117456c57c930a70f
Jonrinck24 <jonrinck220@gmail.com>
2023-06-26 11:04:49 -0600

    userId added to cart

--- db/cart.js
+++ db/cart.js
@@ -2,13 +2,17 @@ const client = require('./client');
 
     async function shoppingCart(userId) {
     try{
+        if(userId){
     const {rows: cart} = await client.query(`
     SELECT products
     FROM cart
     WHERE userId = 1$
             `, [userId])
             return cart;
+        }else{
 
+            return[]
+        }
         }catch (error){
             console.log("error getting cart", error)
             throw error;
@@ -18,17 +22,28 @@ const client = require('./client');
 
     async function addToCart(userId, product ){
         try{
+            if(userId){
         const {rows: product }= await client.query(`
         INSERT INTO cart (userId, productId)
         VAlUES ($1, $2)
         `, [userId, product])
         return updatedCart;
+            } else{
+                throw new Error("User must be logged in too add items to cart")
+            }
+
     } catch (error){
         console.log("error adding to cart", error)
         throw error;
     }
 }
-        
+
+
+
+
+
+
+// user logged in to cart vs non logged in user        
 //add to cart
 //remove from cart
 //update cart
