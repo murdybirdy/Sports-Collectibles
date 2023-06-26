b83e3c71aacb81c3569a320afe230d622eae4cc1
Jonrinck24 <jonrinck220@gmail.com>
2023-06-26 16:03:14 -0600

    started routers in api index.js

--- api/index.js
+++ api/index.js
@@ -11,10 +11,52 @@ apiRouter.get('/health', (req, res, next) => {
     healthy: true,
   });
 });
+apiRouter.get("/unknown", async (req, res, next)=> {
+  res.status(404).ssend({message:"404 not found"})
+})
+
+//Router: user 
+const usersRouter = require('./users');
+router.use('/users', usersRouter);
+
+//Router: products 
+const productsRouter = require('./products');
+router.use('/products', productsRouter);
+
+const cartRouter = require('./cart');
+router.use('/cart', cartRouter);
+
+
+
 
 // place your routers here
   // /users
   // /products
   // /cart
+// / nav bar?
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className = "App" > 
+<Router>
+  <Navbar />
+  <Routes>
+    <Route path = "/" />
+    <Route path ="/users" />
+    <Route path = "/cart" />
+    <Route path = "/products" />
+  </Routes>
+</Router>
+
+</div> */}
+
 
 module.exports = apiRouter;
