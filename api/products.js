65fcd656ef0ffbf06c2077687860c26b4822af50
murdybirdy <murdockdm@outlook.com>
2023-06-26 18:56:44 -0600

    finished (I think) products.js. Will need a way to authenticate admin users to be able to add, edit and delete products

--- api/products.js
+++ api/products.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getAllProducts
+  getAllProducts,
+  getProductById,
+  addProduct,
+  editProduct,
+  removeProduct
 } = require('../db');
 
 // GET /api/products
@@ -12,6 +16,7 @@ router.get('/', async (req, res, next) => {
 
   } catch (error) {
     next(error);
+
   }
 });
 
@@ -19,6 +24,81 @@ router.get('/', async (req, res, next) => {
 router.get('/:productId', async (req, res, next) => {
   const { productId } = req.params;
 
+  try {
+    const product = await getProductById(productId);
+    res.send(product);
+
+  } catch (error) {
+    next(error);
+
+  }
+});
+
+// POST /api/products
+router.post('/', async (req, res, next) => {
+  const { name, category, description, price } = req.body;
+  
+  try {
+    const newProduct = await addProduct({
+      name: name,
+      category: category,
+      description: description,
+      price: price
+    });
+    res.send(newProduct);
+
+  } catch (error) {
+    next(error);
+
+  }
+});
+
+// PATCH /api/products/:productId
+router.patch('/:productId', async (req, res, next) => {
+  const { productId } = req.params;
+  const { name, category, description, price } = req.body;
+  const updateFields = { id: productId };
+
+  if (name) {
+    updateFields.name = name;
+  }
+
+  if (category) {
+    updateFields.category = category;
+  }
+
+  if (description) {
+    updateFields.description = description;
+  }
+
+  if (price) {
+    updateFields.price = price;
+  }
+
+  try {
+    const updatedProduct = await editProduct(updateFields);
+    res.send(updatedProduct);
+
+  } catch (error) {
+    next(error);
+
+  }
+});
+
+// DELETE /api/products/:productId
+router.delete('/:productId', async (req, res, next) => {
+  const { productId } = req.params;
+
+  try {
+    const product = await getProductById(productId);
+
+    await removeProduct(productId);
+    res.send(product);
+    
+  } catch (error) {
+    next(error);
+
+  }
 });
 
 module.exports = router;
