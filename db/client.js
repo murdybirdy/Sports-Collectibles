bbdb76629079c9a60e490b91d49c8716fd054870
Westin <wgneiting1@gmail.com>
2023-07-05 19:17:11 -0600

    frontend skelly

--- db/client.js
+++ db/client.js
@@ -1,8 +1,6 @@
 // Connect to DB
-// const { Client } = require('pg');
 require('dotenv').config();
-const { Pool } = require('pg');
-
+const { Client } = require('pg');
 
 // change the DB_NAME string to whatever your group decides on
 const DB_NAME = 'sporty';
@@ -10,22 +8,6 @@ const DB_NAME = 'sporty';
 const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 
-const client = new Pool({
-  DB_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-});
-
-// github actions client config
-  // if (process.env.CI) {
-  //   client = new Client({
-  //     host: 'localhost',
-  //     port: 5432,
-  //     user: 'postgres',
-  //     password: 'postgres',
-  //     database: 'postgres',
-  //   });
-  // } else {
-  // local / heroku client config
-  // } 
+const client = new Client(DB_URL)
 
 module.exports = client;
---
+++ src/components/Login.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    setIsLoading(true);
+
+    try {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        // Login successful, you can redirect or perform additional actions
+        console.log('Login successful:', data);
+      } else {
+        const data = await response.json();
+        setError(data.error);
+      }
+    } catch (error) {
+      setError('An error occurred. Please try again.');
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {error && <p>{error}</p>}
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? 'Loading...' : 'Login'}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
